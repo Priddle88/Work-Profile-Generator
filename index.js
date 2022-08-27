b13ee894928df8bb6e7af3348e5b995b49d961a9
Parker <parkerriddle09@gmail.com>
2022-08-27 00:18:32 -0600

    Working on prompts and added a sample html page

---
+++ index.js
@@ -0,0 +1,32 @@
+// const Manager = require("./lib/Manager");
+// const Employee = require('./lib/Employee');
+// const inquirer = require('inquirer');
+// const fs = require("fs");
+
+const startApp = require("./lib/startApp");
+
+const start = new startApp();
+
+start.begin();
+
+// const makePage = () => {
+//     `<div class="cards">
+//         <div class="cardHeader">
+//             <h4>${this.name}</h4>
+//             <h5>☕️ ${this.getRole}</h5>
+//         </div>
+//         <div class="cardInfo">
+//             <p>ID: ${this.id}</p>
+//             <p>Email: ${this.email}</p>
+//             <p>Office Number: ${this.officeNumber}</p>
+//         </div>
+//     </div>`
+// }
+
+
+
+// const manager = new Manager(response.name, response.emId, response.email);
+// console.log(manager);
+
+
+
