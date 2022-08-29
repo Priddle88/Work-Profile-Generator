5ab934b789df3feea0e793c8de8c446fa8d9e85f
Parker <parkerriddle09@gmail.com>
2022-08-28 19:06:46 -0600

    Added comments to the javascript

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+//Get the startApp class
 const startApp = require("./lib/startApp");
 
+//Creates a new variable from StartApp
 const start = new startApp();
 
+//Runs the begin method on the new variable 'start'
 start.begin();
